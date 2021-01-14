2b289e3eb36103a1c2ba9dd5849884e1c379797f
CRISTOFER OLLERO ORTIZ <collero@externos.bankia.com>
2021-01-14 13:54:15 +0100

    first commit

---
+++ src/app/pages/heroes/heroes.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { HeroeModel } from '../../models/heroe.model';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: [
+  ]
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: HeroeModel[] = [];
+  cargando = false;
+
+  constructor(private heroesService: HeroesService) { }
+
+  ngOnInit() {
+
+    this.cargando = true;
+    this.heroesService.getHeroes().subscribe(data => {
+      console.log(data);
+      this.heroes = data;
+      this.cargando = false;
+    });
+
+  }
+
+  borrarHeroe(heroe: HeroeModel, i: number) {
+
+    Swal.fire({
+      title: '¿Está seguro?',
+      text: `Está seguro que desea borrar a ${heroe.nombre}`,
+      icon: 'question',
+      showConfirmButton: true,
+      showCancelButton: true
+    }).then(resp => {
+      if (resp.value) {
+        this.heroes.splice(i, 1);
+        this.heroesService.borrarHeroe(heroe.id).subscribe();
+      }
+    });
+
+  }
+
+}
